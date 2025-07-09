ac459859469ef4a3681ad77ff2ddebd934fdc31f
Shirizu <Shidqi354@gmail.com>
2025-07-09 19:48:46 +0700

    Menambahkan paragraf di index.php

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 </head>
 <body>
  <h1>Hello world</h1>
+ <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, libero in. Vel, pariatur repellendus architecto nobis deserunt similique corporis fuga magni aperiam unde reprehenderit deleniti est voluptatum, illo esse delectus.</p>
 </body>
 </html>
