28129e592577c47b01ffaebaea0f5bb3bb4dd7a5
Shirizu <Shidqi354@gmail.com>
2025-07-09 19:53:47 +0700

    Menambhkan list

--- index.php
+++ index.php
@@ -7,5 +7,12 @@
 <body>
  <h1>Hello world</h1>
  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, libero in. Vel, pariatur repellendus architecto nobis deserunt similique corporis fuga magni aperiam unde reprehenderit deleniti est voluptatum, illo esse delectus.</p>
+    <ul>
+        <li>Satu</li>
+        <li>dua</li>
+    </ul>
+
 </body>
+
+
 </html>
